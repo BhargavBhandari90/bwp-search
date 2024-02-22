4ac3b4d98a694ac896d9a9ce1205829410310e31
Bunty <bn.bhandari90@gmail.com>
2024-02-22 19:22:59 +0530

    Build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '8c31a3e7f752eed22683');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '1605eec73239ea3a9bf5');
